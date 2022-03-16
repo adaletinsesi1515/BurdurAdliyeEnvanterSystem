cafaf2801ac3b95fa05aeaa69d68f46d6628b8b8
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-03-16 21:56:18 +0300

    update

--- BurdurAdliyeEnvanterSystem/Formlar/FrmBilgisayarKayit.Designer.cs
+++ BurdurAdliyeEnvanterSystem/Formlar/FrmBilgisayarKayit.Designer.cs
@@ -34,25 +34,25 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.txtid = new DevExpress.XtraEditors.TextEdit();
-            this.lookmarka = new DevExpress.XtraEditors.LookUpEdit();
             this.chkzimmet = new DevExpress.XtraEditors.CheckEdit();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
             this.txtserino = new DevExpress.XtraEditors.TextEdit();
-            this.lookmodel = new DevExpress.XtraEditors.LookUpEdit();
             this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
             this.btnKaydet = new DevExpress.XtraEditors.SimpleButton();
             this.BtnGuncelle = new DevExpress.XtraEditors.SimpleButton();
             this.btnSil = new DevExpress.XtraEditors.SimpleButton();
             this.btnTemizle = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.cmbmarka = new System.Windows.Forms.ComboBox();
+            this.cmbModel = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtid.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lookmarka.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chkzimmet.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtserino.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lookmodel.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl1
@@ -69,6 +69,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             // 
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged);
             // 
             // labelControl1
             // 
@@ -76,7 +77,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.labelControl1.Appearance.Options.UseFont = true;
             this.labelControl1.Location = new System.Drawing.Point(743, 71);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(23, 25);
+            this.labelControl1.Size = new System.Drawing.Size(28, 31);
             this.labelControl1.TabIndex = 1;
             this.labelControl1.Text = "ID ";
             // 
@@ -87,21 +88,8 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.txtid.Name = "txtid";
             this.txtid.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
             this.txtid.Properties.Appearance.Options.UseFont = true;
-            this.txtid.Size = new System.Drawing.Size(80, 30);
+            this.txtid.Size = new System.Drawing.Size(80, 34);
             this.txtid.TabIndex = 2;
-            // 
-            // lookmarka
-            // 
-            this.lookmarka.Location = new System.Drawing.Point(899, 113);
-            this.lookmarka.Name = "lookmarka";
-            this.lookmarka.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
-            this.lookmarka.Properties.Appearance.Options.UseFont = true;
-            this.lookmarka.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.lookmarka.Properties.DisplayMember = "MARKA";
-            this.lookmarka.Properties.ValueMember = "ID";
-            this.lookmarka.Size = new System.Drawing.Size(251, 30);
-            this.lookmarka.TabIndex = 3;
             // 
             // chkzimmet
             // 
@@ -110,7 +98,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.chkzimmet.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 14F);
             this.chkzimmet.Properties.Appearance.Options.UseFont = true;
             this.chkzimmet.Properties.Caption = "Zimmetli mi?";
-            this.chkzimmet.Size = new System.Drawing.Size(149, 29);
+            this.chkzimmet.Size = new System.Drawing.Size(149, 35);
             this.chkzimmet.TabIndex = 4;
             // 
             // labelControl2
@@ -119,7 +107,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.labelControl2.Appearance.Options.UseFont = true;
             this.labelControl2.Location = new System.Drawing.Point(743, 115);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(78, 25);
+            this.labelControl2.Size = new System.Drawing.Size(96, 31);
             this.labelControl2.TabIndex = 5;
             this.labelControl2.Text = "MARKASI";
             // 
@@ -129,7 +117,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.labelControl3.Appearance.Options.UseFont = true;
             this.labelControl3.Location = new System.Drawing.Point(743, 163);
             this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(68, 25);
+            this.labelControl3.Size = new System.Drawing.Size(83, 31);
             this.labelControl3.TabIndex = 6;
             this.labelControl3.Text = "MODELİ";
             // 
@@ -139,7 +127,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.labelControl4.Appearance.Options.UseFont = true;
             this.labelControl4.Location = new System.Drawing.Point(743, 207);
             this.labelControl4.Name = "labelControl4";
-            this.labelControl4.Size = new System.Drawing.Size(135, 25);
+            this.labelControl4.Size = new System.Drawing.Size(166, 31);
             this.labelControl4.TabIndex = 7;
             this.labelControl4.Text = "SERİ NUMARASI";
             // 
@@ -149,21 +137,8 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.txtserino.Name = "txtserino";
             this.txtserino.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
             this.txtserino.Properties.Appearance.Options.UseFont = true;
-            this.txtserino.Size = new System.Drawing.Size(251, 30);
+            this.txtserino.Size = new System.Drawing.Size(251, 34);
             this.txtserino.TabIndex = 8;
-            // 
-            // lookmodel
-            // 
-            this.lookmodel.Location = new System.Drawing.Point(899, 161);
-            this.lookmodel.Name = "lookmodel";
-            this.lookmodel.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
-            this.lookmodel.Properties.Appearance.Options.UseFont = true;
-            this.lookmodel.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.lookmodel.Properties.DisplayMember = "MODEL";
-            this.lookmodel.Properties.ValueMember = "ID";
-            this.lookmodel.Size = new System.Drawing.Size(251, 30);
-            this.lookmodel.TabIndex = 9;
             // 
             // labelControl5
             // 
@@ -173,7 +148,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.labelControl5.Appearance.Options.UseForeColor = true;
             this.labelControl5.Location = new System.Drawing.Point(12, 12);
             this.labelControl5.Name = "labelControl5";
-            this.labelControl5.Size = new System.Drawing.Size(460, 37);
+            this.labelControl5.Size = new System.Drawing.Size(570, 45);
             this.labelControl5.TabIndex = 10;
             this.labelControl5.Text = "BİLGİSAYAR DEPO GİRİŞ KAYIT FORMU";
             // 
@@ -187,6 +162,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.btnKaydet.Size = new System.Drawing.Size(100, 50);
             this.btnKaydet.TabIndex = 11;
             this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
             // BtnGuncelle
             // 
@@ -198,6 +174,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.BtnGuncelle.Size = new System.Drawing.Size(100, 50);
             this.BtnGuncelle.TabIndex = 12;
             this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
             // 
             // btnSil
             // 
@@ -220,26 +197,55 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             this.btnTemizle.Size = new System.Drawing.Size(100, 50);
             this.btnTemizle.TabIndex = 14;
             this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(1108, 256);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 14F);
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Size = new System.Drawing.Size(42, 34);
+            this.textEdit1.TabIndex = 15;
+            // 
+            // cmbmarka
+            // 
+            this.cmbmarka.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbmarka.FormattingEnabled = true;
+            this.cmbmarka.Location = new System.Drawing.Point(899, 114);
+            this.cmbmarka.Name = "cmbmarka";
+            this.cmbmarka.Size = new System.Drawing.Size(251, 36);
+            this.cmbmarka.TabIndex = 16;
+            // 
+            // cmbModel
+            // 
+            this.cmbModel.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbModel.FormattingEnabled = true;
+            this.cmbModel.Location = new System.Drawing.Point(899, 162);
+            this.cmbModel.Name = "cmbModel";
+            this.cmbModel.Size = new System.Drawing.Size(251, 36);
+            this.cmbModel.TabIndex = 17;
             // 
             // FrmBilgisayarKayit
             // 
             this.AcceptButton = this.btnKaydet;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(99)))), ((int)(((byte)(130)))));
             this.ClientSize = new System.Drawing.Size(1268, 989);
+            this.Controls.Add(this.cmbModel);
+            this.Controls.Add(this.cmbmarka);
+            this.Controls.Add(this.textEdit1);
             this.Controls.Add(this.btnTemizle);
             this.Controls.Add(this.btnSil);
             this.Controls.Add(this.BtnGuncelle);
             this.Controls.Add(this.btnKaydet);
             this.Controls.Add(this.labelControl5);
-            this.Controls.Add(this.lookmodel);
             this.Controls.Add(this.txtserino);
             this.Controls.Add(this.labelControl4);
             this.Controls.Add(this.labelControl3);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.chkzimmet);
-            this.Controls.Add(this.lookmarka);
             this.Controls.Add(this.txtid);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.gridControl1);
@@ -252,10 +258,9 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtid.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lookmarka.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chkzimmet.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtserino.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lookmodel.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -267,17 +272,18 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.TextEdit txtid;
-        private DevExpress.XtraEditors.LookUpEdit lookmarka;
         private DevExpress.XtraEditors.CheckEdit chkzimmet;
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.LabelControl labelControl3;
         private DevExpress.XtraEditors.LabelControl labelControl4;
         private DevExpress.XtraEditors.TextEdit txtserino;
-        private DevExpress.XtraEditors.LookUpEdit lookmodel;
         private DevExpress.XtraEditors.LabelControl labelControl5;
         private DevExpress.XtraEditors.SimpleButton btnKaydet;
         private DevExpress.XtraEditors.SimpleButton BtnGuncelle;
         private DevExpress.XtraEditors.SimpleButton btnSil;
         private DevExpress.XtraEditors.SimpleButton btnTemizle;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.ComboBox cmbmarka;
+        private System.Windows.Forms.ComboBox cmbModel;
     }
 }
--- BurdurAdliyeEnvanterSystem/Formlar/FrmBilgisayarKayit.cs
+++ BurdurAdliyeEnvanterSystem/Formlar/FrmBilgisayarKayit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.XtraEditors.Repository;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,7 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
 
         DbBurdurAdliyeEntities db = new DbBurdurAdliyeEntities();
 
-        void Listeleme ()
+        void Listeleme()
         {
             var deger = from x in db.TBLBILGISAYARLAR
                         select new
@@ -27,16 +28,98 @@ namespace BurdurAdliyeEnvanterSystem.Formlar
                             x.ID,
                             MARKA = x.TBLMARKALAR.MARKAADI,
                             MODEL = x.TBLMODELLER.MODELADI,
-                            x.SERINO,                            
+                            x.SERINO,
                             x.STATUS,
                             x.ZIMMET
                         };
-            
+
             gridControl1.DataSource = deger.ToList();
         }
         private void FrmBilgisayarKayit_Load(object sender, EventArgs e)
         {
+            
+            Listeleme();
+            lookmarka.Properties.DataSource = (from x in db.TBLMARKALAR
+                                               select new
+                                               {
+                                                   x.ID,
+                                                   x.MARKAADI
+                                               }).ToList();
+
+            lookmodel.Properties.DataSource = (from x in db.TBLMODELLER
+                                               select new
+                                               {
+                                                   x.ID,
+                                                   MARKA = x.TBLMARKALAR.MARKAADI,
+                                                   x.MODELADI
+                                               }).ToList();
+            lookmarka.Properties.NullText = "Marka Seçiniz";
+            lookmodel.Properties.NullText = "Model Seçiniz";
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+
+            txtid.Text = gridView1.GetFocusedRowCellValue("ID").ToString();
+            txtserino.Text = gridView1.GetFocusedRowCellValue("SERINO").ToString();
+            textEdit1.Text = gridView1.GetFocusedRowCellValue("ZIMMET").ToString();
+            textEdit1.Visible = false;
+            if ((textEdit1.Text == "True")) chkzimmet.Checked = true;
+            if ((textEdit1.Text == "False")) chkzimmet.Checked = false;
+
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtid.Text = "";
+            txtserino.Text = "";
+            lookmarka.Text = "";
+            lookmodel.Text = "";
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            TBLBILGISAYARLAR t = new TBLBILGISAYARLAR();
+
+            t.MARKAID = byte.Parse(lookmarka.EditValue.ToString());
+            t.MODELID = byte.Parse(lookmodel.EditValue.ToString());
+            t.SERINO = txtserino.Text;
+            t.STATUS = true;
+            if (chkzimmet.Checked == true)
+            {
+                t.ZIMMET = true;
+            }
+            else
+            {
+                t.ZIMMET = false;
+            }
+
+            db.TBLBILGISAYARLAR.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Bilgisayar başarıyla kaydedildi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Listeleme();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtid.Text);
+            var deger = db.TBLBILGISAYARLAR.Find(id);
+            deger.MARKAID = byte.Parse(cmbmarka.Text);
+            deger.MODELID= byte.Parse(lookmodel.EditValue.ToString());
+            deger.SERINO= txtserino.Text;
+            if (chkzimmet.Checked == true)
+            {
+                deger.ZIMMET = true;
+            }
+            else
+            {
+                deger.ZIMMET = false;
+            }
+            db.SaveChanges();
+            MessageBox.Show("Bilgisayar kaydı güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
             Listeleme();
         }
     }
 }
+
